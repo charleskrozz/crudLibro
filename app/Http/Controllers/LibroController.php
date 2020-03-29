02f0877676fed79f8222c600900e1cb6cc58d26d
unknown <charleskrozz007@gmail.com>
2020-03-28 20:33:00 -0500

    subir el proyecto crudLibro

---
+++ app/Http/Controllers/LibroController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Libro;
+class LibroController extends Controller
+{
+
+    public function index()
+    {
+        $libros = Libro::orderBy('id','DESC')->paginate(3);
+        return view('libro.index',compact('libros'));
+    }
+
+
+    public function create()
+    {
+        return view('libro.create');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request,['nombre'=>'required','resumen'=>'required','npagina'=>'required','edicion'=>'required','autor'=>'required','precio'=>'required']);
+        Libro::create($request->all());
+        return redirect()->route('libro.index')->with('success','Registro creado');
+    }
+
+    public function show($id)
+    {
+        $libro = Libro::find($id);
+        return view('libro.show',compact('libros'));
+    }
+
+    public function edit($id)
+    {
+        $libro = Libro::find($id);
+        return view('libro.edit', compact('libro'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $this->validate($request,['nombre'=>'required','resumen'=>'required','npagina'=>'required','edicion'=>'required','autor'=>'required','precio'=>'required']);
+        Libro::find($id)->update($request->all());
+        return redirect()->route('libro.index')->with('success','registro actualziado!');
+    }
+
+    public function destroy($id)
+    {
+        Libro::find($id)->delete();
+        return redirect()->route('libro.index')->with('success','Registro eliminado satisfactoriamente');
+
+    }
+}
